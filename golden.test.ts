ced76f44a31ee8b37eddcc899d96cd26f9bb7a9c
RAZRULETEL <qwerty.qwertlain@gmail.com>
2024-04-28 21:51:59 +0300

    added cat, hello and hello_user tests, golden test rewritten to async fs

--- golden.test.ts
+++ golden.test.ts
@@ -1,3 +1,4 @@
+import * as Crypto from "crypto";
 import * as fs from "fs";
 import {SourceProgram} from "./translator-types";
 import {execSync} from "child_process";
@@ -11,8 +12,6 @@ interface GoldenTestConfig {
 }
 
 const TEST_TEMP_DIR = 'tmp';
-const TEST_OUTPUT_SOURCE = `${TEST_TEMP_DIR}/output.json`;
-const TEST_INPUT_STDIN = `${TEST_TEMP_DIR}/input`;
 const TEST_PROCESSOR_TIME_LIMIT = 1_000_000;
 
 const TRANSLATOR_PATH = "./translator.js";
@@ -20,24 +19,39 @@ const PROCESSOR_PATH = "./model/processor.js";
 
 describe('Running golden tests', () => {
     it.each([
-        "tests/factorial.json",
-        "tests/prob1.json",
         "tests/hello.json",
-    ])(`from file %p`, (test_config: string) => {
+        "tests/cat.json",
+        "tests/hello_user.json",
+        "tests/prob1.json",
+        "tests/factorial.json"
+    ])(`from file %p`, async (test_config: string) => {
         const config = JSON.parse(fs.readFileSync(test_config, "utf8")) as GoldenTestConfig;
 
-        if (!fs.existsSync(TEST_TEMP_DIR))
-            fs.mkdirSync(TEST_TEMP_DIR);
+        const TEMP_DIR = `tmp/test_${Crypto.randomUUID()}`;
+        const OUTPUT_SOURCE = `${TEMP_DIR}/output.json`;
+        const INPUT_STDIN = `${TEMP_DIR}/input`;
 
-        fs.writeFileSync(`${TEST_TEMP_DIR}/input.lisp`, config.input_lisp.join(""));
-        fs.writeFileSync(TEST_INPUT_STDIN, config.input_stdin);
+        try {
+            await fs.promises.mkdir(TEMP_DIR, { recursive: true });
+        }catch (err) {
+            // ignore, directory already exists
+        }
 
-        execSync(`node ${TRANSLATOR_PATH} ${TEST_TEMP_DIR}/input.lisp ${TEST_OUTPUT_SOURCE}`);
-        expect(JSON.parse(fs.readFileSync(TEST_OUTPUT_SOURCE, 'utf8'))).toEqual(config.output_json);
+        await fs.promises.writeFile(`${TEMP_DIR}/input.lisp`, config.input_lisp.join(""));
+        await fs.promises.writeFile(INPUT_STDIN, config.input_stdin);
 
-        const stdout = execSync(`node ${PROCESSOR_PATH} ${TEST_OUTPUT_SOURCE} ${TEST_INPUT_STDIN} ${TEST_PROCESSOR_TIME_LIMIT}`);
+        execSync(`node ${TRANSLATOR_PATH} ${TEMP_DIR}/input.lisp ${OUTPUT_SOURCE}`);
+        expect(JSON.parse(await fs.promises.readFile(OUTPUT_SOURCE, 'utf8'))).toEqual(config.output_json);
+
+        const stdout = execSync(`node ${PROCESSOR_PATH} ${OUTPUT_SOURCE} ${INPUT_STDIN} ${TEST_PROCESSOR_TIME_LIMIT}`);
         expect(stdout.toString()).toEqual(config.output_stdout);
 
-        fs.rmSync(TEST_TEMP_DIR, { force: true, recursive: true });
+        await fs.promises.rm(TEMP_DIR, { recursive: true, force: true });
+        try {
+            await fs.promises.rmdir(TEST_TEMP_DIR);
+        }catch (err) {
+            // ignore, not last test
+        }
     })
 });
+
